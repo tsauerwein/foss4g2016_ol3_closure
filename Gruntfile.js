b8c43eaec6defbe79dcb504022841588ea65b61d
tsauerwein <tobias.sauerwein@camptocamp.com>
2016-08-09 21:42:23 +0200

    Some more changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
       local: {
         // local files with no pre-processing
         expand: true,
-        src: ['ol3-closure.html', 'img/**/*.*', 'theme/**/*.*'],
+        src: ['*.html', 'img/**/*.*', 'theme/**/*.*'],
         dest: build
       },
       examples: {
